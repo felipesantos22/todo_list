b1b0c69df941d68d41b260ef7ec3dabe799ebb90
felipesantos22 <felipevs2018@gmail.com>
2023-11-20 10:42:40 -0300

    feat: insert ssl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
